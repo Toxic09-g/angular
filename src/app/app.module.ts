60799dbf83558a4ecf03119a4d51074f6458b639
Роман Платонов <platonov_rye1605@mail.ru>
2021-12-13 14:05:21 +0600

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { RegComponent } from './reg/reg.component';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { UsersComponent } from './users/users.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+	{ path: '', component: LoginComponent },
+	{ path: 'main', component: MainComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'reg', component: RegComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RouterModule]
 })
 export class AppModule { }
